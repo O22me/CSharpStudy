35e6bc0e9cf68841d7549cbc52a3e8e29efca34e
김세훈\김세훈PC <sehoon0926@enscape.co.kr>
2023-02-02 13:32:38 +0900

    초기 커밋.

---
+++ ThisIsCSharp/ExtensionMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtension;
+
+namespace MyExtension
+{
+    public static class IntegerExtension
+    {
+        public static double Square(this double myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int myInt, int exponent)
+        {
+            int result = myInt;
+            for (int i = 1; i < exponent; i++)
+                result = result * myInt;
+            return result;
+        }
+    }
+}
+
+namespace CSharpTester
+{
+    class ExtensionMethod
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"3.141592^2 : {3.141592.Square()}");
+        }
+    }
+}
---
+++ ThisIsCSharp/Indexer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTester
+{
+    class MyList<T>
+    {
+        private T[] array;
+
+        public MyList()
+        {
+            array = new T[3];
+        }
+
+        public T this[int index]  //인덱서
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array resized : {array.Length}");
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+    }
+
+    class Indexer
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+
+            list[0] = "abc";
+            list[1] = "def";
+            list[2] = "ghi";
+            list[3] = "jkl";
+            list[4] = "mno";
+
+            for (int i = 0; i < list.Length; i++)
+                Console.WriteLine(list[i]);
+        }
+    }
+}
